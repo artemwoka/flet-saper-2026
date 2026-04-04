b1d2e28d6bdb42546542cba0cf9a91cdbfb8c2a6
artemwoka <artemwoka@gmail.com>
2026-04-04 17:19:01 +0200

    Enhance MineSweeper functionality by refining cell reveal logic, correcting variable names, and implementing mine calculation and UI updates

--- saper.py
+++ saper.py
@@ -15,6 +15,18 @@ STATUS_PLAY = 1
 STATUS_FAILED = 2
 STATUS_SUCCESS = 3
 
+# Кольори цифр (кількість мін навколо клітинки)
+Num_COLORS = {
+    1: ft.Colors.BLUE,
+    2: ft.Colors.GREEN_800,
+    3: ft.Colors.RED,
+    4: ft.Colors.INDIGO,
+    5: ft.Colors.BROWN_700,
+    6: ft.Colors.TEAL,
+    7: ft.Colors.BLACK,
+    8: ft.Colors.GREY_600,
+}
+
 CELL_SIZE = 30
 
 
@@ -28,7 +40,7 @@ class Cell:
 
     def reset(self):
         self.is_mine = False
-        self.is_opened = False
+        self.is_revealed = False
         self.is_flagged = False
         self.is_start = False
         self.is_end = False
@@ -48,7 +60,7 @@ class MineSweeper:
         self.board_size, self.mines_count = LEVELS[self.level]
 
         self.cells: list[list[Cell]] = []
-        self.cell_cointainers: list[list[ft.Container]] = []
+        self.cell_containers: list[list[ft.Container]] = []
 
         self._build_ui()
         self.reset()
@@ -74,7 +86,7 @@ class MineSweeper:
     def _build_grid(self):
         """Побудова ігрового поля"""
         self.cells = []
-        self.cell_cointainers = []
+        self.cell_containers = []
         self.grid_column.controls.clear()
 
         for x in range(self.board_size):
@@ -86,22 +98,49 @@ class MineSweeper:
                 cell = Cell(x, y)
                 row_cells.append(cell)
 
-                cointainer = ft.Container(
+                container = ft.Container(
                     width = CELL_SIZE,
                     height = CELL_SIZE,
                     bgcolor = ft.Colors.BLUE_GREY_300,
                     border_radius = 2,
-                    border = ft.border.all(1, ft.Colors.BLUE_GREY_500),
+                    border = ft.Border.all(1, ft.Colors.BLUE_GREY_500),
                     alignment=ft.Alignment.CENTER
 
                 )
 
-                row_containers.append(cointainer)
-                row.controls.append(cointainer)
+                gesture = ft.GestureDetector(
+                    content=container,
+                    on_tap=lambda e, cx=x, cy=y: self._on_cell_tap(cx, cy)
+                )
+
+                row_containers.append(container)
+                row.controls.append(gesture)
 
             self.cells.append(row_cells)
-            self.cell_cointainers.append(row_containers)
+            self.cell_containers.append(row_containers)
             self.grid_column.controls.append(row)
+    
+    def _calc_mines_around(self):
+        """Обчислення кількості мін навколо кожної клітинки"""
+        for x, y, cell in self._get_all_cells():
+            if not cell.is_mine:
+                cell.mines_around = sum(
+                    1 for _, _, c in self._get_neighbors(x, y) if c.is_mine
+                )
+                #self.cell_cointainers[x][y].content = ft.Text(str(cell.mines_around))
+
+    def _expand_reveal(self, x: int, y: int):
+        """Розккриття порожніх клітинок методом заливки (BFS)"""
+        queue = [(x, y )]
+        while queue:
+            cx, cy =queue.pop(0)
+            for xi, yi, cell in self._get_neighbors(cx, cy):
+                if not cell.is_revealed and not cell.is_mine and not cell.is_flagged:
+                    cell.is_revealed = True
+                    self._update_cell_ui(cell)
+                    if cell.mines_around == 0:
+                        queue.append((xi, yi)) 
+
 
     def _get_all_cells(self):
         """Генератор для отримання всіх клітинок"""
@@ -118,6 +157,30 @@ class MineSweeper:
                     result.append((xi, yi, self.cells[xi][yi]))
         return result
 
+    def _reveal_cell(self, cell: Cell):
+        """Розкриття однієї клітинки"""
+        if cell.is_revealed or cell.is_flagged:
+            return
+
+        cell.is_revealed = True
+        self._update_cell_ui(cell)
+
+        if cell.is_mine:
+            cell.is_end = True
+            self._update_cell_ui(cell)
+            return
+        
+        if cell.mines_around == 0:
+            self._expand_reveal(cell.x, cell.y)
+    
+    def _on_cell_tap(self, x: int, y: int):
+        """Обробка лівого кліку по клітинці"""
+        cell = self.cells[x][y]
+        if not cell.is_revealed:
+            self._reveal_cell(cell)
+        self.page.update()
+            
+
     def _set_mines(self):
         """Випадкове розміщення мін на полі"""
         position = set()
@@ -126,18 +189,36 @@ class MineSweeper:
             y = random.randint(0, self.board_size - 1)
             if (x, y) not in position:
                 self.cells[x][y].is_mine = True
-                self.cell_cointainers[x][y].content = ft.Text("💣")
+                #self.cell_cointainers[x][y].content = ft.Text("💣")
                 position.add((x, y))
                 
-    
-    def _calc_mines_around(self):
-        """Обчислення кількості мін навколо кожної клітинки"""
-        for x, y, cell in self._get_all_cells():
-            if not cell.is_mine:
-                cell.mines_around = sum(
-                    1 for _, _, c in self._get_neighbors(x, y) if c.is_mine
+
+    def _update_cell_ui(self, cell: Cell):
+        """Оновлення зовнішнього вигляду клітинки"""
+        container = self.cell_containers[cell.x][cell.y]
+
+        if cell.is_revealed:
+            if cell.is_end:
+                container.bgcolor = ft.Colors.RED_300
+                container.content = ft.Text("💥", size=14)
+            elif cell.is_mine:
+                container.bgcolor = ft.Colors.GREY_300
+                container.content = ft.Text("💣", size=14)
+            elif cell.mines_around > 0:
+                container.bgcolor = ft.Colors.GREY_200
+                container.content = ft.Text(
+                    str(cell.mines_around),
+                    size=16,
+                    weight=ft.FontWeight.BOLD,
+                    color=Num_COLORS.get(cell.mines_around, ft.Colors.BLACK)
                 )
-                self.cell_cointainers[x][y].content = ft.Text(str(cell.mines_around))
+            else:
+                container.bgcolor = ft.Colors.GREY_200
+                container.content = None
+        else:
+            container.bgcolor = ft.Colors.BLUE_GREY_300
+            container.content = None
+
 
 def main(page: ft.Page):
     MineSweeper(page)
